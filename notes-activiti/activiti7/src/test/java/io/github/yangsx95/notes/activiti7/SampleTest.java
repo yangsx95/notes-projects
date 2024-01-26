869c31e0c2252fade8125c6cd873bd44c4423dd0
杨顺翔 <yangsx95@qq.com>
2024-01-26 16:04:08 +0800

    feat(activiti7): 工作流引擎的基本操作

--- notes-activiti/activiti7/src/test/java/io/github/yangsx95/notes/activiti7/SampleTest.java
+++ notes-activiti/activiti7/src/test/java/io/github/yangsx95/notes/activiti7/SampleTest.java
@@ -5,12 +5,11 @@ import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.history.HistoricProcessInstance;
 import org.activiti.engine.repository.Deployment;
-import org.activiti.engine.repository.DeploymentQuery;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -34,7 +33,7 @@ public class SampleTest {
         RepositoryService repositoryService = engine.getRepositoryService();
         Deployment deployment = repositoryService.createDeployment()
                 .name("请假申请单")
-                .addClasspathResource("bpmn/leave.bpmn")
+                .addClasspathResource("bpmn/leave.bpmn20.xml")
                 .key(DEPLOY_KEY)
                 .deploy();
         Assertions.assertNotNull(deployment);
@@ -59,11 +58,47 @@ public class SampleTest {
         Assertions.assertNotNull(processInstance);
         System.out.println("流程实例信息：" + processInstance);
 
-        // 查询该流程实例的所有任务
+        // 查询该流程实例要完成的所有任务
         List<Task> taskList = engine.getTaskService().createTaskQuery()
                 .processInstanceId(processInstance.getId())
                 .list();
         Assertions.assertFalse(taskList.isEmpty());
+
+        // 最开始系应该有一个任务，zhangsan 创建请假单
+        Assertions.assertEquals(taskList.get(0).getName(), "创建请假单");
+
+        // 查询zhangsan下的任务
+        Task zhangsanTask = engine.getTaskService().createTaskQuery()
+                .taskCandidateOrAssigned("zhangsan")
+                .processInstanceId(processInstance.getId())
+                .singleResult();
+        Assertions.assertNotNull(zhangsanTask);
+        Assertions.assertEquals(zhangsanTask.getName(), "创建请假单");
+
+        // zhangsan完成任务，下一个就是lisi，部门经理审核
+        engine.getTaskService().complete(zhangsanTask.getId());
+
+        Task lisiTask = engine.getTaskService().createTaskQuery()
+                .taskCandidateOrAssigned("lisi")
+                .processInstanceId(processInstance.getId())
+                .singleResult();
+        engine.getTaskService().complete(lisiTask.getId());
+        Assertions.assertNotNull(lisiTask);
+        Assertions.assertEquals(lisiTask.getName(), "部门经理审核");
+
+        Task wangwuTask = engine.getTaskService().createTaskQuery()
+                .taskCandidateOrAssigned("wangwu")
+                .processInstanceId(processInstance.getId())
+                .singleResult();
+        engine.getTaskService().complete(wangwuTask.getId());
+        Assertions.assertNotNull(wangwuTask);
+        Assertions.assertEquals(wangwuTask.getName(), "人事复核");
+
+        // wangwu复核之后，该流程应该就结束了，进入到了历史表中
+        HistoricProcessInstance historicProcessInstance = engine.getHistoryService().createHistoricProcessInstanceQuery().processInstanceId(processInstance.getId()).singleResult();
+        Assertions.assertNotNull(historicProcessInstance);
+        Assertions.assertEquals(historicProcessInstance.getProcessDefinitionName(), "请假单");
+        Assertions.assertEquals(historicProcessInstance.getProcessDefinitionKey(), "leave");
 
         // 删除流程以及关联的流程实例，参数2会级联删除关联的其他数据
         repositoryService.deleteDeployment(deployment.getId(), true);

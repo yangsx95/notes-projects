0ed3b7ad68624d9da13864c23e4885c412c22fc3
杨顺翔 <yangsx95@qq.com>
2024-03-25 09:01:10 +0800

    seata at

--- notes-seata/.conf/init-db/pay-init.sql
+++ notes-seata/.conf/init-db/pay-init.sql
@@ -6,3 +6,16 @@ create table IF NOT EXISTS `pay`.t_account
     account_balance decimal(11, 2) not null comment '账户余额',
     constraint t_product_pk primary key (id)
 );
+CREATE TABLE IF NOT EXISTS `pay`.`undo_log`
+(
+    `branch_id`     BIGINT       NOT NULL COMMENT 'branch transaction id',
+    `xid`           VARCHAR(128) NOT NULL COMMENT 'global transaction id',
+    `context`       VARCHAR(128) NOT NULL COMMENT 'undo_log context,such as serialization',
+    `rollback_info` LONGBLOB     NOT NULL COMMENT 'rollback info',
+    `log_status`    INT(11)      NOT NULL COMMENT '0:normal status,1:defense status',
+    `log_created`   DATETIME(6)  NOT NULL COMMENT 'create datetime',
+    `log_modified`  DATETIME(6)  NOT NULL COMMENT 'modify datetime',
+    UNIQUE KEY `ux_undo_log` (`xid`, `branch_id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4 COMMENT ='AT transaction mode undo table';
+ALTER TABLE `pay`.`undo_log` ADD INDEX `ix_log_created` (`log_created`);
+
--- notes-seata/.conf/init-db/product-init.sql
+++ notes-seata/.conf/init-db/product-init.sql
@@ -7,3 +7,15 @@ create table IF NOT EXISTS `product`.t_product
     constraint t_product_pk primary key (id)
 );
 
+CREATE TABLE IF NOT EXISTS `product`.`undo_log`
+(
+    `branch_id`     BIGINT       NOT NULL COMMENT 'branch transaction id',
+    `xid`           VARCHAR(128) NOT NULL COMMENT 'global transaction id',
+    `context`       VARCHAR(128) NOT NULL COMMENT 'undo_log context,such as serialization',
+    `rollback_info` LONGBLOB     NOT NULL COMMENT 'rollback info',
+    `log_status`    INT(11)      NOT NULL COMMENT '0:normal status,1:defense status',
+    `log_created`   DATETIME(6)  NOT NULL COMMENT 'create datetime',
+    `log_modified`  DATETIME(6)  NOT NULL COMMENT 'modify datetime',
+    UNIQUE KEY `ux_undo_log` (`xid`, `branch_id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4 COMMENT ='AT transaction mode undo table';
+ALTER TABLE `product`.`undo_log` ADD INDEX `ix_log_created` (`log_created`);
